c3b71159be6f29338efe368676135e3d68facdd8
Đoàn Văn Minh <minhdv@yogame.vn>
2023-06-07 01:56:49 +0700

    first commit

---
+++ desktop/src/com/dbdc/game/DesktopLauncher.java
@@ -0,0 +1,18 @@
+package com.dbdc.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.dbdc.game.DBDCGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("danh-bai-dai-cuong");
+		config.setWindowedMode(800, 480);
+		config.useVsync(true);
+//		new Lwjgl3Application(new Drop(), config);
+		new Lwjgl3Application(new Basic3DTest(), config);
+	}
+}
